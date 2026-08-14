86b522c2eff2377ba03912c94c9332eb0edb5500
random23967 <Luke.Odgers@my.mrc.tas.edu.au>
2026-08-14 04:10:29 +0000

    finnished

--- 01-cafe-pos/tier2_beginner/01_order_one_item.py
+++ 01-cafe-pos/tier2_beginner/01_order_one_item.py
@@ -43,7 +43,9 @@ MENU = {
 
 # TODO 1: ask the customer what they would like, using input(), and store
 #         their answer in a variable called order_item
-
+customer_order = input("What items would you like to request at this fine establishment that we call a cafe")
+if customer_order in MENU:
+    print(f'good job you have orderd a {customer_order} a classic of this fine establishment')
 # TODO 2: check whether order_item exists in MENU
 #         - if it does: print "Great choice! {order_item} costs ${price}."
 #         - if it doesn't: print "Sorry, we don't have {order_item} on the menu today."
